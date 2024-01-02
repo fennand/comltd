03fba8d84b820bc387fa603186ca3382dd1f2445
Andrew Fennell <afennell@hotmail.co.uk>
2024-01-02 15:54:35 +0000

    added _document.tsx file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>{/* Additional head tags go here */}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

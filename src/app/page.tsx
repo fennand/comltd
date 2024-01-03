c7ad67731a02793adf3ee3f7991581ee4dff3b0e
Andrew Fennell <afennell@hotmail.co.uk>
2024-01-03 14:10:59 +0000

    commented out images on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ const Page = () => {
     <main className="flex flex-col items-center justify-center bg-white">
       {/* Logo and Title */}
       <div className="text-center mb-8">
-        <img
+        {/* <img
           src="/logo_image.png"
           alt="Logo image"
           width={300}
           height={300}
           className="mx-auto"
-        />
+        /> */}
         <h1 className="text-3xl font-bold mt-4">
           Welcome to Commoditology Ltd
         </h1>
@@ -20,13 +20,13 @@ const Page = () => {
 
       {/* Laptop Image and First Paragraph */}
       <div className="flex flex-row items-center mb-8">
-        <img
+        {/* <img
           src="/laptop.jpg"
           alt="laptop image"
           width={400}
           height={400}
           className="mr-4"
-        />
+        /> */}
         <p className="max-w-md text-xl">
           Commoditology Limited specializes in trading commodities in the global
           marketplace. We have a professional and experienced team who have been
@@ -43,13 +43,13 @@ const Page = () => {
           vel mauris consectetur eleifend. Maecenas quis velit eu nisi auctor
           eleifend. Nam sed nisl sed massa lacinia bibendum. Nam fermentum ac.
         </p>
-        <img
+        {/* <img
           src="/buildings.jpg"
           alt="buildings image"
           width={400}
           height={400}
           className="ml-4"
-        />
+        /> */}
       </div>
     </main>
   );
